dc74300892b5f6e959a69b22fa2d0c5f78bedfde
Florian Sittel <sittel@lettis.net>
2014-07-11 09:28:00 +0200

    added data to unit test and adapted path

--- cpp/test_read_coords.cpp
+++ cpp/test_read_coords.cpp
@@ -10,7 +10,7 @@ int main() {
   std::size_t n_rows;
   std::size_t n_cols;
 
-  std::tie(c, n_rows, n_cols) = read_coords("test.xyz", usecols = {0, 2, 4});
+  std::tie(c, n_rows, n_cols) = read_coords("test/coords.dat", usecols = {0, 2, 4});
 
   for (std::size_t i=0; i < n_rows; ++i) {
     for (std::size_t j=0; j < n_cols; ++j) {
