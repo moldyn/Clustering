d55a23d9fcb8a7c989045a62b3ebbcb9974cfc07
Florian Sittel <sittel@lettis.net>
2014-07-14 16:54:00 +0200

    prepare merge

--- cpp/density_clustering.cpp
+++ cpp/density_clustering.cpp
@@ -144,6 +144,9 @@ nearest_neighbor(const CoordsPointer<float>& coords_pointer,
   std::vector<float> distances(search_range.second-search_range.first);
   #pragma omp parallel for default(shared) private(dist,j,c,d) firstprivate(n_cols)
   for (j=search_range.first; j < search_range.second; ++j) {
+    if (frame_id == j) {
+      continue;
+    }
     dist = 0.0f;
     for (c=0; c < n_cols; ++c) {
       d = coords[sorted_density[frame_id].first*n_cols+c] - coords[sorted_density[j].first*n_cols+c];
@@ -190,7 +193,7 @@ density_clustering(const std::vector<float>& dens,
   // compute sigma as standard deviation of nearest-neighbor distances
   float sigma = 0.0f;
   for (std::size_t i=0; i < n_rows; ++i) {
-    if (i % 1000 == 0) std::cout << i << " / " << n_rows << std::endl;
+    if (i % 1000 == 0) std::cout << i << " / " << n_rows << "; sum(sigma): " << sigma << std::endl;
     sigma += nearest_neighbor(coords_pointer, density_sorted, n_cols, i, {0,n_rows}).second;
   }
   sigma /= n_rows;
