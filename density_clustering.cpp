8b9e61073a7361b35b749fdd9b9fbb54571a74d0
Florian Sittel <sittel@lettis.net>
2014-08-03 17:42:10 +0200

    corrected: STL-map -> STL-set in comment

--- density_clustering.cpp
+++ density_clustering.cpp
@@ -286,7 +286,7 @@ density_clustering(const std::vector<float>& dens,
             // indeed, there are already cluster assignments.
             // these should now be merged under a common name.
             // (which will be the id with smallest numerical value,
-            //  due to the properties of STL-maps).
+            //  due to the properties of STL-sets).
             common_name = (*cluster_names.begin());
           } else {
             // no clustering of these frames yet.
