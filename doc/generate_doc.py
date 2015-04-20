6a515bb19f097e789499d91ff8948a734ba720a9
Florian Sittel <sittel@lettis.net>
2015-04-20 22:32:01 +0200

    formatting improvements

--- doc/generate_doc.py
+++ doc/generate_doc.py
@@ -9,41 +9,40 @@ def formatted(html, latex, filetype):
     raise Exception("UNKNOWN FORMAT")
 
 def header(title, filetype):
-  html = """
-    <!DOCTYPE html>
-    <html>
-    <head>
-    <meta charset="UTF-8">
-    <title>%s</title>
-    <style type="text/css">
-    body{
-      font-family: Verdana, sans-serif;
-      background-color: #FFFFFF;
-    }
-    h1, h2, h3, h4 {
-      padding: 10px;
-      border: 0px;
-      margin: 0px;
-      text-align: center;
-      font-variant: small-caps;
-    }
-    h1 {
-      background-color: #CCCCCC;
-      padding: 25px;
-    }
-    h2 {
-      background-color: #DDFFDD;
-    }
-    div {
-      background-color: #EEEEEE;
-      padding: 20px;
-    }
-    </style>
-    </head>
-    <body>
-    <h1>%s</h1>"""
+  html = """<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>%s</title>
+<style type="text/css">
+body{
+  font-family: Verdana, sans-serif;
+  background-color: #FFFFFF;
+}
+h1, h2, h3, h4 {
+  padding: 10px;
+  border: 0px;
+  margin: 0px;
+  text-align: center;
+  font-variant: small-caps;
+}
+h1 {
+  background-color: #CCCCCC;
+  padding: 25px;
+}
+h2 {
+  background-color: #DDFFDD;
+}
+div {
+  background-color: #EEEEEE;
+  padding: 20px;
+}
+</style>
+</head>
+<body>
+<h1>%s</h1>"""
   latex = """
-    TODO: %s
+TODO: %s
   """
   return formatted(html % (title, title)
                  , latex % title
@@ -55,29 +54,29 @@ def footer(filetype):
   return formatted(html, latex, filetype)
 
 def section(title, filetype):
-  html = """<h2>%s</h2>"""
-  latex = """\section{%s}"""
+  html = "<h2>%s</h2>\n"
+  latex = "\section{%s}\n"
   return formatted(html % title
                  , latex % title
                  , filetype)
 
 def subsection(title, filetype):
-  html = """<h3>%s</h3>"""
-  latex = """\subsection{%s}"""
+  html = "<h3>%s</h3>\n"
+  latex = "\subsection{%s}\n"
   return formatted(html % title
                  , latex % title
                  , filetype)
 
 def emph(content, filetype):
-  html = """<i>%s</i>"""
-  latex = """\emph{%s}"""
+  html = "<i>%s</i>"
+  latex = "\emph{%s}"
   return formatted(html % content
                  , latex % content
                  , filetype)
 
 def content(content, filetype):
-  html = "<div>%s</div>"
-  latex = "%s"
+  html = "<div>%s</div>\n"
+  latex = "%s\n"
   return formatted(html % content
                  , latex % content
                  , filetype)
